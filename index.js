0c42729002003c9c2befeffb87aa037951d4afb5
raihan-778 <raihanarif778@gmail.com>
2022-11-24 15:14:30 +0600

    mongodb database has been created

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// mongodb database setup
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const usedLaptopCollections = client
+  .db("reselledProductsHub")
+  .collection("LaptopCollections");
+// perform actions on the collection object
+
 app.get("/", (req, res) => {
   res.send("Used Product Resel server is running Perfectly");
 });
